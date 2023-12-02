2ccfbef90cb792ebab686e948c395a3e688d4e39
Daniel Björk <daniel_bjork@live.se>
2023-12-02 13:42:09 +0100

    Add project files.

---
+++ 01-Trebuchet/Program.cs
@@ -0,0 +1,18 @@
+﻿var part1 = File.ReadAllLines("input.txt")
+    .Select(x => x.Where(char.IsDigit))
+    .Select(x => int.Parse(x.First() + "" + x.Last()))
+    .Sum();
+
+Console.WriteLine(part1);
+
+List<string> nums = ["_", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+
+var part2 = File.ReadAllLines("input.txt")
+    .Select(ParseNumbers)
+    .Select(x => x[0] * 10 + x[^1])
+    .Sum();
+
+Console.WriteLine(part2);
+
+int[] ParseNumbers(string s) =>
+    s.Select((x, i) => x < 58 ? x - 48 : nums.FindIndex(s[i..].StartsWith)).Where(x => x > 0).ToArray();
---
+++ 02-Cubes/Program.cs
@@ -0,0 +1,37 @@
+﻿var part1 = File.ReadAllLines("input.txt")
+    .Select(MaxCubes)
+    .Where(x => x.Red <= 12 && x.Green <= 13 && x.Blue <= 14)
+    .Sum(x => x.Id);
+
+Console.WriteLine(part1);
+
+var part2 = File.ReadAllLines("input.txt")
+    .Select(MaxCubes)
+    .Select(x => x.Red * x.Green * x.Blue)
+    .Sum();
+
+Console.WriteLine(part2);
+
+(int Id, int Red, int Green, int Blue) MaxCubes(string line, int index)
+{
+    var rounds = line.Split([':',';']);
+    int maxRed = 0, maxGreen = 0, maxBlue = 0;
+
+    foreach (string r in rounds[1..])
+    {
+        var parts = r.Split([' ', ',']);
+
+        for (int i = 1; i < parts.Length; i += 3)
+        {
+            int num = int.Parse(parts[i]);
+
+            if (parts[i + 1] == "red")
+                maxRed = Math.Max(maxRed, num);
+            else if (parts[i + 1] == "green")
+                maxGreen = Math.Max(maxGreen, num);
+            else if (parts[i + 1] == "blue")
+                maxBlue = Math.Max(maxBlue, num);
+        }
+    }
+    return (index + 1, maxRed, maxGreen, maxBlue);
+}
