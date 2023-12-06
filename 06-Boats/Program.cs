cc72a6c0aecda78f61e9faa76f20e41c0118899d
Daniel Björk <daniel_bjork@live.se>
2023-12-06 14:34:26 +0100

    Day 6.

---
+++ 06-Boats/Program.cs
@@ -0,0 +1,29 @@
+﻿
+var input = File.ReadAllLines("input.txt").Select(x => x.Split(' ', StringSplitOptions.RemoveEmptyEntries)[1..]).ToArray();
+var times = input[0].Select(long.Parse).ToArray();
+var records = input[1].Select(long.Parse).ToArray();
+
+long part1 = 1;
+for (int i = 0; i < times.Length; i++)
+    part1 *= Wins(times[i], records[i]);
+
+Console.WriteLine(part1);
+
+
+var time = long.Parse(string.Concat(input[0]));
+var record = long.Parse(string.Concat(input[1]));
+long part2 = Wins(time, record);
+
+Console.WriteLine(part2);
+
+
+long Wins(long time, long distance)
+{
+    long wins = 0;
+    for(long i = 1; i < time - 1; i++)
+    {
+        if((time - i) * i > distance)
+            wins++;
+    }
+    return wins;
+}
